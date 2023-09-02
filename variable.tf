75996ad0d128c6606556f283e9c4b4eefe2fe722
johnmeera <143019004+johnmeera@users.noreply.github.com>
2023-09-02 17:34:09 +0530

    variable.tf

--- variable.tf
+++ variable.tf
@@ -2,8 +2,3 @@ variable "mybucketname" {
   description = "bucket name my wish"
   type        =  string
 }
-variable "access" {
-  description = "bucket name my wish"
-  type        =  string
-  default = "private"
-}
